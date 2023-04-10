1a06b3fe7cd92612e7af6b9180362bdced0edc9a
Evgeny Hyvärinen <61843193+EvgenijHyva@users.noreply.github.com>
2023-04-10 23:00:25 +0300

    Memory optimizations

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,11 @@
 use wasm_bindgen::prelude::*;
+use wee_alloc::WeeAlloc;
 
-// exporting into webassembly 
+// Memory optimizations
+#[global_allocator]
+static ALLOC: WeeAlloc = WeeAlloc::INIT;
+
+// Exporting into WebAssembly 
 #[wasm_bindgen]
 pub fn greet(name: &str) {
 	println!("{}",name);
